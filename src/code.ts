41a4691f35abfc719dc62d5c009b3dbbe507214b
clement dauptain <dauptain.c@gmail.com>
2019-03-19 23:38:51 +0100

    create observable from event

--- src/code.ts
+++ src/code.ts
@@ -1,29 +1,12 @@
-import {Observable} from "rxjs/Observable";
-import 'rxjs/add/operator/share';
+import { fromEvent } from 'rxjs/Observable/fromEvent';
 
-var observable = Observable.create((observer: any) => {
-    try {
-        observer.next('Hello world');
-        observer.next('Comment ca va?');
-        setInterval(() => {
-            observer.next('Tout vas bien');
-        }, 2000);
-    } catch(err) {
-        observer.error(err);
-    }
-}).share();
-
-var observer = observable.subscribe(
-    (x:any) => addItem(x),
-    (error:any) => addItem(error),
-    () => addItem('COMPLETED')
-);
+var observable = fromEvent(document, 'mousemove');
 
 setTimeout(() => {
-    var observer2 = observable.subscribe(
-        (x:any) => addItem('Subscriber 2: ' + x)
+    var subscription = observable.subscribe(
+        (x:any) => addItem(x)
     )
-}, 1000);
+}, 2000);
 
 function addItem(val: any) {
     var node = document.createElement("li");
